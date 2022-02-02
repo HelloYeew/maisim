92e46ecc659f1ddb77bb2e45ff284e031957c1a2
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-03 04:41:24 +0700

    Update test scene to use setup

--- maisim/maisim.Game.Tests/Visual/TestSceneBackgroundScreen.cs
+++ maisim/maisim.Game.Tests/Visual/TestSceneBackgroundScreen.cs
@@ -1,11 +1,13 @@
-﻿using osu.Framework.Graphics;
+﻿using NUnit.Framework;
+using osu.Framework.Graphics;
 using osu.Framework.Screens;
 
 namespace maisim.Game.Tests.Visual
 {
     public class TestSceneBackgroundScreen : maisimTestScene
     {
-        public TestSceneBackgroundScreen()
+        [SetUp]
+        public void SetUp()
         {
             Add(new ScreenStack(new BackgroundScreen()) {RelativeSizeAxes = Axes.Both});
         }
