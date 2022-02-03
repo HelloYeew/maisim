2e6e95209141fcf36001f195cde218b35a33d6c1
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-04 00:46:09 +0700

    Add test scene on MainMenuButton

---
+++ maisim/maisim.Game.Tests/Visual/MainMenu/TestSceneMainMenuButton.cs
@@ -0,0 +1,37 @@
+using maisim.Game.Component;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osuTK;
+using osuTK.Graphics;
+
+namespace maisim.Game.Tests.Visual.MainMenu
+{
+    public class TestSceneMainMenuButton : maisimTestScene
+    {
+        public TestSceneMainMenuButton()
+        {
+            Child = new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(300),
+                Masking = true,
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Color4.DimGray
+                    },
+                    new MainMenuButton("Test", FontAwesome.Solid.Smile)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    }
+                }
+            };
+        }
+    }
+}
