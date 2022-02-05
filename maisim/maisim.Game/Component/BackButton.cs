ac1c48a9bd7dccdc3827f89166444a6963da1305
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-05 22:56:27 +0700

    Add sound sample event for BackButton

--- maisim/maisim.Game/Component/BackButton.cs
+++ maisim/maisim.Game/Component/BackButton.cs
@@ -1,19 +1,26 @@
 using osu.Framework.Allocation;
+using osu.Framework.Audio.Sample;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Audio;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Input.Bindings;
+using osu.Framework.Input.Events;
 using osuTK;
-using osuTK.Graphics;
 
 namespace maisim.Game.Component
 {
     public class BackButton : VisibilityContainer
     {
+
+        // TODO: Select between CompositeDrawable or VisibilityContainer
+
+        private DrawableSample drawableHoverSample;
+        private DrawableSample drawableClickSample;
+
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(ISampleStore sampleStore)
         {
             InternalChildren = new Drawable[]
             {
@@ -39,6 +46,9 @@ namespace maisim.Game.Component
                     }
                 }
             };
+
+            drawableHoverSample = new DrawableSample(sampleStore.Get("hover.wav"));
+            drawableClickSample = new DrawableSample(sampleStore.Get("click2.wav"));
         }
 
         protected override void PopIn()
@@ -49,6 +59,18 @@ namespace maisim.Game.Component
         protected override void PopOut()
         {
             this.FadeOut(400, Easing.OutQuint);
+        }
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            drawableHoverSample.Play();
+            return base.OnHover(e);
+        }
+
+        protected override bool OnClick(ClickEvent e)
+        {
+            drawableClickSample.Play();
+            return base.OnClick(e);
         }
     }
 }
