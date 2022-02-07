a5fb534161fd851108387b81c17bba580a284f25
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-08 04:19:32 +0700

    Make a test scene for testing on a new song selection background

---
+++ maisim/maisim.Game.Tests/Visual/Component/TestSceneSongSelectionBackground.cs
@@ -0,0 +1,32 @@
+﻿using maisim.Game.Component;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osuTK;
+
+namespace maisim.Game.Tests.Visual.Component
+{
+    public class TestSceneSongSelectionBackground : maisimTestScene
+    {
+        public TestSceneSongSelectionBackground()
+        {
+            Child = new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(400, 400),
+                RelativePositionAxes = Axes.X,
+                Children = new Drawable[]
+                {
+                    new SongSelectionBackground(Color4Extensions.FromHex("fb5f99"))
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativePositionAxes = Axes.Both,
+                        RelativeSizeAxes = Axes.Both,
+                    }
+                }
+            };
+        }
+    }
+}
--- maisim/maisim.Game/Screen/SongSelectionScreen.cs
+++ maisim/maisim.Game/Screen/SongSelectionScreen.cs
@@ -1,25 +1,40 @@
 using maisim.Game.Component;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Screens;
 using osuTK;
+using osuTK.Graphics;
 
-namespace maisim.Game
+namespace maisim.Game.Screen
 {
-    public class SongSelectionScreen : Screen
+    public class SongSelectionScreen : osu.Framework.Screens.Screen
     {
         [BackgroundDependencyLoader]
         private void load()
         {
             InternalChildren = new Drawable[]
             {
-                new BackButton
+                new Container
                 {
-                    Anchor = Anchor.BottomLeft,
+                    Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
-                    Size = new Vector2(0.5f),
-                    RelativeSizeAxes = Axes.Both,
-                },
+                    Size = new Vector2(400,400),
+                    RelativePositionAxes = Axes.X,
+                    Children = new Drawable[]
+                    {
+                        new SongSelectionBackground(Color4Extensions.FromHex("fb5f99"))
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            RelativePositionAxes = Axes.Both,
+                            RelativeSizeAxes = Axes.Both,
+                        }
+                    }
+                }
             };
         }
     }
