59f8889acb09762444604db417a8a8b35d385ee7
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-08 04:17:56 +0700

    Finish implement on SongSelection background

---
+++ maisim/maisim.Game/Component/SongSelectionBackground.cs
@@ -0,0 +1,47 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Textures;
+using osuTK;
+using osuTK.Graphics;
+
+namespace maisim.Game.Component
+{
+    public class SongSelectionBackground : CompositeDrawable
+    {
+        private readonly Color4 backgroundColor;
+
+        public SongSelectionBackground(Color4 backgroundColor)
+        {
+            this.backgroundColor = backgroundColor;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    RelativePositionAxes = Axes.Both,
+                    RelativeSizeAxes = Axes.X,
+                    Colour = backgroundColor,
+                    Size = new Vector2(400,400)
+                },
+                new Circle
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    RelativePositionAxes = Axes.Both,
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = backgroundColor,
+                    Size = new Vector2(1.5f)
+                }
+            };
+        }
+    }
+}
