dd563ff89fc24ca9d7318ac3026fc4ad8ffc65e7
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-08 19:26:55 +0700

    Add mock-up TrackCardFocus to the screen

--- maisim/maisim.Game/Screen/SongSelectionScreen.cs
+++ maisim/maisim.Game/Screen/SongSelectionScreen.cs
@@ -3,11 +3,7 @@ using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Screens;
 using osuTK;
-using osuTK.Graphics;
 
 namespace maisim.Game.Screen
 {
@@ -22,7 +18,7 @@ namespace maisim.Game.Screen
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
-                    Size = new Vector2(400,400),
+                    Size = new Vector2(320,320),
                     RelativePositionAxes = Axes.X,
                     Children = new Drawable[]
                     {
@@ -32,6 +28,23 @@ namespace maisim.Game.Screen
                             Origin = Anchor.Centre,
                             RelativePositionAxes = Axes.Both,
                             RelativeSizeAxes = Axes.Both,
+                        },new Container
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            RelativePositionAxes = Axes.Both,
+                            RelativeSizeAxes = Axes.Both,
+                            Children = new Drawable[]
+                            {
+                                new TrackCardFocused("Test/sukino-skill.jpg", "Sukino Skill", "Wake Up, Girls!",
+                                    100.6969f, "SSS", 1278, 2424, true, true, "HelloYeew", 120)
+                                {
+                                    Anchor = Anchor.TopCentre,
+                                    Origin = Anchor.TopCentre,
+                                    RelativePositionAxes = Axes.Both,
+                                    Size = new Vector2(300,384)
+                                }
+                            }
                         }
                     }
                 }
