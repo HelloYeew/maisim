e5df8bee20fd7f5af1ffd6d5885991f91c9324d9
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-12 03:25:12 +0700

    Finish add the ring endpoint of each lane

--- maisim/maisim.Game/Component/Gameplay/MaisimRing.cs
+++ maisim/maisim.Game/Component/Gameplay/MaisimRing.cs
@@ -1,3 +1,4 @@
+using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -9,25 +10,61 @@ namespace maisim.Game.Component.Gameplay
 {
     public class MaisimRing : CircularContainer
     {
+        private static readonly float[] lane_angles =
+        {
+            22.5f,
+            67.5f,
+            112.5f,
+            157.5f,
+            202.5f,
+            247.5f,
+            292.5f,
+            337.5f
+        };
+
+        private static readonly float LANE_MULTIPLIER = 284.5f; // TODO: Still not sure on this. But when the outline circle is working, this should be changed too.
+
         [BackgroundDependencyLoader]
         private void load()
         {
             Anchor = Anchor.Centre;
             Origin = Anchor.Centre;
-            Size = new Vector2(250, 250);
+            Size = new Vector2(580, 580);
             Children = new Drawable[]
             {
-                new Circle()
+                new Circle
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                     RelativeSizeAxes = Axes.Both,
-                    Colour = Color4.White,
+                    Colour = Color4.Pink,
                     BorderThickness = 10,
-                    BorderColour = Color4.Pink,
+                    BorderColour = Color4.Green,
                     Masking = true
                 }
             };
+
+            foreach (float angle in lane_angles)
+            {
+                AddInternal(new Circle
+                {
+                    Position = new Vector2(
+                        -(LANE_MULTIPLIER * (float)Math.Cos((angle + 90f) * (float)(Math.PI / 180))),
+                        -(LANE_MULTIPLIER * (float)Math.Sin((angle + 90f) * (float)(Math.PI / 180)))
+                    ),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(10)
+                });
+            }
+
+            AddInternal(new Circle
+            {
+                Position = Vector2.Zero,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(10),
+            });
         }
     }
 }
