f40e6bd2b8a619034fdff9356259107c2bf496ac
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-22 17:54:50 +0700

    Make a class for return the color value for each difficulty

---
+++ maisim/maisim.Game/Graphics/MaisimColour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using maisim.Game.Beatmaps;
+using maisim.Game.Component;
+using osu.Framework.Extensions.Color4Extensions;
+using osuTK.Graphics;
+
+namespace maisim.Game.Graphics
+{
+    public class MaisimColour
+    {
+        /// <summary>
+        /// Get the colour for <see cref="TrackCard"/> and <see cref="TrackCardFocused"/> background colour.
+        /// </summary>
+        /// <param name="difficultyRating"><see cref="DifficultyRating"/> value</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException">Throw if the value is not right.</exception>
+
+        public Color4 GetDifficultyColor(DifficultyRating difficultyRating)
+        {
+            switch (difficultyRating)
+            {
+                case DifficultyRating.Basic:
+                    return Color4Extensions.FromHex("6ed43e");
+
+                case DifficultyRating.Advanced:
+                    return Color4Extensions.FromHex("f7b807");
+
+                case DifficultyRating.Expert:
+                    return Color4Extensions.FromHex("ff828d");
+
+                case DifficultyRating.Master:
+                    return Color4Extensions.FromHex("a051dc");
+
+                case DifficultyRating.Remaster:
+                    return Color4.White;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(difficultyRating));
+            }
+        }
+    }
+}
