cc0b311795bca858a3bc7da18ab488fb43094ef6
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-22 18:31:58 +0700

    Add description for each test case on track card visual test

--- maisim/maisim.Game.Tests/Visual/Component/TestSceneTrackCardFocusedManyStyle.cs
+++ maisim/maisim.Game.Tests/Visual/Component/TestSceneTrackCardFocusedManyStyle.cs
@@ -1,50 +1,95 @@
 ﻿using maisim.Game.Beatmaps;
 using maisim.Game.Component;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Testing;
 using osuTK;
 
-namespace maisim.Game.Tests.Visual.Component
+namespace maisim.Game.Tests.Visual.Component;
+
+public class TestSceneTrackCardFocusedManyStyle : GridTestScene
 {
-    public class TestSceneTrackCardFocusedManyStyle : GridTestScene
+    public TestSceneTrackCardFocusedManyStyle() : base(2, 3)
     {
-        public TestSceneTrackCardFocusedManyStyle() : base(2,3)
+        Cell(0, 0).Children = new Drawable[]
         {
-            Cell(0, 0).Child = new TrackCardFocused("Test/sukino-skill.jpg", "Sukino Skill", "Wake Up, Girls!",
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Basic",
+            },
+            new TrackCardFocused("Test/sukino-skill.jpg", "Sukino Skill", "Wake Up, Girls!",
                 100.6969f, "SSS", 1278, 2424, true, true, "HelloYeew", 120, DifficultyRating.Basic)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Scale = new Vector2(0.7f)
-            };
-            Cell(0, 1).Child = new TrackCardFocused("Test/lemon.jpg", "Lemon", "Kenshi Yonezu",
+            }
+        };
+        Cell(0, 1).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Advanced",
+            },
+            new TrackCardFocused("Test/lemon.jpg", "Lemon", "Kenshi Yonezu",
                 100.2900f, "SS", 58, 4542, false, true, "PepePoggers", 80, DifficultyRating.Advanced)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Scale = new Vector2(0.7f)
-            };
-            Cell(0,2).Child = new TrackCardFocused("Test/only-my-railgun.jpg", "only my railgun", "fripSide",
+            }
+        };
+        Cell(0, 2).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Expert",
+            },
+            new TrackCardFocused("Test/only-my-railgun.jpg", "only my railgun", "fripSide",
                 100.00f, "SS", 797, 4215, true, false, "Kasumi", 190, DifficultyRating.Expert)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Scale = new Vector2(0.7f)
-            };
-            Cell(1,0).Child = new TrackCardFocused("Test/raise-my-sword.jpg", "RAISE MY SWORD", "GALNERYUS",
+            }
+        };
+        Cell(1, 0).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Master",
+            },
+            new TrackCardFocused("Test/raise-my-sword.jpg", "RAISE MY SWORD", "GALNERYUS",
                 0, "", 0, 4279, false, false, "AmPen", 220, DifficultyRating.Master)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Scale = new Vector2(0.7f)
-            };
-            Cell(1,1).Child = new TrackCardFocused("Test/tenkai-e-no-kippu.jpg", "Tenkai e no Kippu", "Dragon Guardian",
+            }
+        };
+        Cell(1, 1).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Remaster"
+            },
+            new TrackCardFocused("Test/tenkai-e-no-kippu.jpg", "Tenkai e no Kippu", "Dragon Guardian",
                 0, "", 0, 4279, false, false, "Kroytz", 190, DifficultyRating.Remaster)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Scale = new Vector2(0.7f)
-            };
-        }
+            }
+        };
     }
 }
--- maisim/maisim.Game.Tests/Visual/Component/TestSceneTrackCardManyStyle.cs
+++ maisim/maisim.Game.Tests/Visual/Component/TestSceneTrackCardManyStyle.cs
@@ -1,45 +1,95 @@
 ﻿using maisim.Game.Beatmaps;
 using maisim.Game.Component;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Testing;
 using osuTK;
 
-namespace maisim.Game.Tests.Visual.Component
+namespace maisim.Game.Tests.Visual.Component;
+
+public class TestSceneTrackCardManyStyle : GridTestScene
 {
-    public class TestSceneTrackCardManyStyle : GridTestScene
+    public TestSceneTrackCardManyStyle() : base(2, 3)
     {
-        public TestSceneTrackCardManyStyle() : base(2,3)
+        Cell(0, 0).Children = new Drawable[]
         {
-            Cell(0, 0).Child = new TrackCard("Test/sukino-skill.jpg", "Sukino Skill", "Wake Up, Girls!",
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Basic",
+            },
+            new TrackCard("Test/sukino-skill.jpg", "Sukino Skill", "Wake Up, Girls!",
                 100.6969f, "SSS", 1278, 2424, true, true, "HelloYeew", 120, DifficultyRating.Basic)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-            };
-            Cell(0, 1).Child = new TrackCard("Test/lemon.jpg", "Lemon", "Kenshi Yonezu",
+                Scale = new Vector2(0.7f)
+            }
+        };
+        Cell(0, 1).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Advanced",
+            },
+            new TrackCard("Test/lemon.jpg", "Lemon", "Kenshi Yonezu",
                 100.2900f, "SS", 58, 4542, false, true, "PepePoggers", 80, DifficultyRating.Advanced)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-            };
-            Cell(0,2).Child = new TrackCard("Test/only-my-railgun.jpg", "only my railgun", "fripSide",
+                Scale = new Vector2(0.7f)
+            }
+        };
+        Cell(0, 2).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Expert",
+            },
+            new TrackCard("Test/only-my-railgun.jpg", "only my railgun", "fripSide",
                 100.00f, "SS", 797, 4215, true, false, "Kasumi", 190, DifficultyRating.Expert)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-            };
-            Cell(1,0).Child = new TrackCard("Test/raise-my-sword.jpg", "RAISE MY SWORD", "GALNERYUS",
+                Scale = new Vector2(0.7f)
+            }
+        };
+        Cell(1, 0).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Master",
+            },
+            new TrackCard("Test/raise-my-sword.jpg", "RAISE MY SWORD", "GALNERYUS",
                 0, "", 0, 4279, false, false, "AmPen", 220, DifficultyRating.Master)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-            };
-            Cell(1,1).Child = new TrackCard("Test/tenkai-e-no-kippu.jpg", "Tenkai e no Kippu", "Dragon Guardian",
+                Scale = new Vector2(0.7f)
+            }
+        };
+        Cell(1, 1).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = "DifficultyRating.Remaster"
+            },
+            new TrackCard("Test/tenkai-e-no-kippu.jpg", "Tenkai e no Kippu", "Dragon Guardian",
                 0, "", 0, 4279, false, false, "Kroytz", 190, DifficultyRating.Remaster)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-            };
-        }
+                Scale = new Vector2(0.7f)
+            }
+        };
     }
 }
