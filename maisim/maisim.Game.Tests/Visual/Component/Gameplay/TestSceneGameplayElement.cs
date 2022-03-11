20d03a5a2a7b581a339005227d46fa1d4b2dcb6c
Phawit Pornwattanakul <me@helloyeew.dev>
2022-03-12 02:17:16 +0700

    Add test scene to test the size of element

---
+++ maisim/maisim.Game.Tests/Visual/Component/Gameplay/TestSceneGameplayElement.cs
@@ -0,0 +1,125 @@
+﻿using maisim.Game.Component.Gameplay.Notes;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Testing;
+using osuTK;
+
+namespace maisim.Game.Tests.Visual.Component.Gameplay;
+
+public class TestSceneGameplayElement : GridTestScene
+{
+    public TestSceneGameplayElement() : base(4, 3)
+    {
+        Cell(0, 0).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(Tap)
+            },
+            new Tap
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+
+        Cell(0, 1).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(TapBoth)
+            },
+            new TapBoth
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+
+        Cell(0, 2).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(Break)
+            },
+            new Break
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+
+        Cell(1, 0).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(SlideStar)
+            },
+            new SlideStar
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+
+        Cell(1, 1).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(SlidePath)
+            },
+            new SlidePath
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+
+        Cell(2, 0).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(Touch)
+            },
+            new Touch
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+
+        Cell(3, 0).Children = new Drawable[]
+        {
+            new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Text = nameof(Touch)
+            },
+            new TouchHold
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.5f)
+            }
+        };
+    }
+}
