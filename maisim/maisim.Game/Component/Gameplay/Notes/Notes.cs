e352ce379d7fbc5def158d0df229f7b9279e4e6e
Phawit Pornwattanakul <me@helloyeew.dev>
2022-03-12 01:49:37 +0700

    Reimplement `Notes` abstract class

--- maisim/maisim.Game/Component/Gameplay/Notes/Notes.cs
+++ maisim/maisim.Game/Component/Gameplay/Notes/Notes.cs
@@ -1,38 +1,9 @@
-﻿using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Textures;
-using osuTK;
+﻿using osu.Framework.Graphics.Containers;
 
 namespace maisim.Game.Component.Gameplay.Notes
 {
     public abstract class Notes : CompositeDrawable
     {
-        private string TextureName;
 
-        protected Notes(string textureName)
-        {
-            TextureName = textureName;
-        }
-
-        [BackgroundDependencyLoader]
-        private void load(TextureStore textureStore)
-        {
-            InternalChild = new Container
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(50),
-                Child = new Sprite
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    FillMode = FillMode.Fill,
-                    Texture = textureStore.Get(TextureName)
-                }
-            };
-        }
     }
 }
--- maisim/maisim.Game/Component/Gameplay/Notes/SlidePath.cs
+++ maisim/maisim.Game/Component/Gameplay/Notes/SlidePath.cs
@@ -1,10 +1,31 @@
-﻿namespace maisim.Game.Component.Gameplay.Notes
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osuTK;
+
+namespace maisim.Game.Component.Gameplay.Notes
 {
     public class SlidePath : Notes
     {
-        public SlidePath(string textureName = "Notes/SlidePath.png") : base(textureName)
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textureStore)
         {
-
+            InternalChild = new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(50),
+                Child = new Sprite
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    FillMode = FillMode.Fill,
+                    Texture = textureStore.Get("Notes/SlidePath.png")
+                }
+            };
         }
     }
 }
