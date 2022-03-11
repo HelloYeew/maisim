7b3cf9176a4430591e34fead5a3c3aa56751e8d2
Phawit Pornwattanakul <me@helloyeew.dev>
2022-03-12 02:17:05 +0700

    Finish TouchHold and Break note

---
+++ maisim/maisim.Game/Component/Gameplay/Notes/TouchHold.cs
@@ -0,0 +1,31 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osuTK;
+
+namespace maisim.Game.Component.Gameplay.Notes
+{
+    public class TouchHold : Notes
+    {
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textureStore)
+        {
+            InternalChild = new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(50),
+                Child = new Sprite
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    FillMode = FillMode.Fill,
+                    Texture = textureStore.Get("Notes/TouchHold.png")
+                }
+            };
+        }
+    }
+}
