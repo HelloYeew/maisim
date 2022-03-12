bad3c9fa8f869be0740cd1939d48d03112045976
Phawit Pornwattanakul <me@helloyeew.dev>
2022-03-13 01:39:53 +0700

    Add GlobalBeatmap bindable

---
+++ maisim/maisim.Game.Tests/Visual/Component/TestSceneGlobalBeatmap.cs
@@ -0,0 +1,64 @@
+﻿using maisim.Game.Beatmaps;
+using maisim.Game.Component;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+
+namespace maisim.Game.Tests.Visual.Component;
+
+public class TestSceneGlobalBeatmap : maisimTestScene
+{
+    private GlobalBeatmap mockGlobalBeatmap;
+    private TrackMetadata dummyTrackMetadata;
+    private Bindable<TrackMetadata> currentWorkingTrackMetadata;
+    private Bindable<TrackMetadata> currentBindTrackMetadata;
+
+    public TestSceneGlobalBeatmap()
+    {
+        mockGlobalBeatmap = new GlobalBeatmap();
+        dummyTrackMetadata = new TrackMetadata
+        {
+            Title = "FREEDOM DiVE↓",
+            Artist = "xi",
+            CoverPath = "Test/parousia.jpg",
+            Bpm = 0
+        };
+        currentBindTrackMetadata.SetDefault();
+
+        Children = new Drawable[]
+        {
+            new CurrentWorkingBeatmapTest()
+        };
+    }
+
+    public class CurrentWorkingBeatmapTest : CompositeDrawable
+    {
+        private string title;
+        private string artist;
+        private string coverPath;
+        private float bpm;
+        private TextFlowContainer currentWorkingBeatmapText;
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            title = "";
+            artist = "";
+            coverPath = "";
+            bpm = 0;
+
+            InternalChildren = new Drawable[]
+            {
+                currentWorkingBeatmapText = new TextFlowContainer
+                {
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    RelativeSizeAxes = Axes.Both,
+                    Text = "Title : " + title + "\nArtist : " + artist + "\nCoverPath : " + coverPath + "\nBpm : " + bpm
+                }
+            };
+        }
+    }
+}
---
+++ maisim/maisim.Game/Component/GlobalBeatmap.cs
@@ -0,0 +1,34 @@
+﻿using maisim.Game.Beatmaps;
+using osu.Framework.Bindables;
+
+namespace maisim.Game.Component
+{
+    public class GlobalBeatmap : osu.Framework.Graphics.Component
+    {
+        private TrackMetadata blankTrackMetadata;
+
+        public Bindable<TrackMetadata> currentGlobalBeatmap;
+
+        public GlobalBeatmap()
+        {
+            blankTrackMetadata = new TrackMetadata
+            {
+                Title = "No working beatmap!",
+                Artist = "Please select a beatmap!",
+                CoverPath = "blankbeatmap.png",
+                Bpm = 0
+            };
+            currentGlobalBeatmap = new Bindable<TrackMetadata>(blankTrackMetadata);
+        }
+
+        public void SetGlobalBeatmap(TrackMetadata trackMetadata)
+        {
+            currentGlobalBeatmap.Value = trackMetadata;
+        }
+
+        public TrackMetadata GetGlobalBeatmap()
+        {
+            return currentGlobalBeatmap.Value;
+        }
+    }
+}
