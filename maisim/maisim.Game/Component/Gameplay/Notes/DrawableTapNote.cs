ab28dbafbdd342d4e72a1aac6b4511e93b5ddc26
Phawit Pornwattanakul <me@helloyeew.dev>
2022-03-26 05:41:24 +0700

    Move syntax to spawn note to inside the note class

--- maisim/maisim.Game/Component/Gameplay/Notes/DrawableTapNote.cs
+++ maisim/maisim.Game/Component/Gameplay/Notes/DrawableTapNote.cs
@@ -1,6 +1,10 @@
-﻿using osu.Framework.Graphics;
+﻿using System;
+using maisim.Game.Screen.Gameplay;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
+using osuTK;
 
 namespace maisim.Game.Component.Gameplay.Notes
 {
@@ -9,8 +13,22 @@ namespace maisim.Game.Component.Gameplay.Notes
     /// </summary>
     public class DrawableTapNote : DrawableNote
     {
-
         public NoteLane Lane { get; set; }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+            Position = new Vector2(
+                -(Playfield.SPAWNER_MULTIPLIER *
+                  (float)Math.Cos((NoteLaneExtension.GetAngle(Lane) + 90f) * (float)(Math.PI / 180))),
+                -(Playfield.SPAWNER_MULTIPLIER *
+                  (float)Math.Sin((NoteLaneExtension.GetAngle(Lane) + 90f) * (float)(Math.PI / 180)))
+            );
+            Scale = new Vector2(1.2f);
+            Margin = new MarginPadding(10);
+        }
 
         protected override Drawable[] AddNoteParts(TextureStore textureStore)
         {
