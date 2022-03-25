2435f75c13834b254b447614e71e0b6b6c09fa18
Phawit Pornwattanakul <me@helloyeew.dev>
2022-03-26 05:42:03 +0700

    Add test scene for test to spawn note in time

---
+++ maisim/maisim.Game.Tests/Visual/Screen/TestSceneNoteSpawnTimer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using maisim.Game.Component.Gameplay.Notes;
+using maisim.Game.Screen.Gameplay;
+using maisim.Game.Utils;
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osuTK;
+
+namespace maisim.Game.Tests.Visual.Screen;
+
+public class TestSceneNoteSpawnTimer : maisimTestScene
+{
+    private PlayfieldScreen playfieldScreen;
+
+    private Random random = new Random();
+
+    [SetUp]
+    public void SetUp()
+    {
+        Add(playfieldScreen = new PlayfieldScreen {RelativeSizeAxes = Axes.Both});
+
+        playfieldScreen.Playfield.NotesPool = new List<DrawableNote>
+        {
+            new DrawableTapNote()
+            {
+                Lane = NoteLane.Lane1,
+                TargetTime = 5000f
+            },
+            new DrawableTapNote()
+            {
+                Lane = NoteLane.Lane2,
+                TargetTime = 7000f
+            },
+            new DrawableTapNote()
+            {
+                Lane = NoteLane.Lane3,
+                TargetTime = 9000f
+            },
+            new DrawableTapNote()
+            {
+                Lane = NoteLane.Lane4,
+                TargetTime = 11000f
+            },
+            new DrawableTapNote()
+            {
+                Lane = NoteLane.Lane5,
+                TargetTime = 13000f
+            },
+            new DrawableTapNote()
+            {
+                Lane = NoteLane.Lane6,
+                TargetTime = 15000f
+            },
+        };
+    }
+}
