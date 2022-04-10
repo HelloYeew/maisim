816dc6b35f7f80dd79603cd81f709785c499d111
Phawit Pornwattanakul <me@helloyeew.dev>
2022-04-11 01:17:10 +0700

    Move update note position logic to the method

--- maisim/maisim.Game/Screen/Gameplay/Playfield.cs
+++ maisim/maisim.Game/Screen/Gameplay/Playfield.cs
@@ -62,6 +62,44 @@ namespace maisim.Game.Screen.Gameplay
             });
         }
 
+        /// <summary>
+        /// Update the target <see cref="DrawableNote"/>'s position.
+        /// </summary>
+        /// <param name="tapNote">The <see cref="DrawableTapNote"/> that want to update.</param>
+        private void updateTapNotePosition(DrawableTapNote tapNote)
+        {
+            if (MathUtils.EuclideanDistance(NoteLaneExtension.GetSpawnerPosition(tapNote.Lane), NoteLaneExtension.GetSensorPosition(tapNote.Lane)) + DISTANCE_ON_DESPAWN <
+                MathUtils.EuclideanDistance(tapNote.Position, NoteLaneExtension.GetSpawnerPosition(tapNote.Lane)))
+            {
+                // Enter despawning state
+                tapNote.FadeOut(50, Easing.InBounce);
+                Scheduler.AddDelayed(() => RemoveInternal(tapNote), 500);
+            }
+            else
+            {
+                if (tapNote.TargetTime != 0f)
+                {
+                    double speed = MathUtils.EuclideanDistance(
+                        NoteLaneExtension.GetSpawnerPosition(tapNote.Lane),
+                        NoteLaneExtension.GetSensorPosition(tapNote.Lane)) / TIME_NOTE_APPEARS;
+                    tapNote.Position = new Vector2(
+                        (float) (tapNote.Position.X + ((float)(speed * (-(float)Math.Cos((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) * (float)(Math.PI / 180)))) * Clock.ElapsedFrameTime)),
+                        (float) (tapNote.Position.Y + ((float)(speed * (-(float)Math.Sin((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) * (float)(Math.PI / 180)))) * Clock.ElapsedFrameTime))
+                        );
+                }
+                else
+                {
+                    // Update the position
+                    tapNote.Position += new Vector2(
+                        -(NOTE_SPEED * (float)Math.Cos((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) *
+                                                       (float)(Math.PI / 180))),
+                        -(NOTE_SPEED * (float)Math.Sin((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) *
+                                                       (float)(Math.PI / 180)))
+                    );
+                }
+            }
+        }
+
         protected override void Update()
         {
             if (NotesPool != null)
@@ -90,36 +128,7 @@ namespace maisim.Game.Screen.Gameplay
             {
                 if (note is DrawableTapNote tapNote)
                 {
-                    if (MathUtils.EuclideanDistance(NoteLaneExtension.GetSpawnerPosition(tapNote.Lane), NoteLaneExtension.GetSensorPosition(tapNote.Lane)) + DISTANCE_ON_DESPAWN <
-                        MathUtils.EuclideanDistance(tapNote.Position, NoteLaneExtension.GetSpawnerPosition(tapNote.Lane)))
-                    {
-                        // Enter despawning state
-                        note.FadeOut(50, Easing.InBounce);
-                        Scheduler.AddDelayed(() => RemoveInternal(note), 500);
-                    }
-                    else
-                    {
-                        if (tapNote.TargetTime != 0f)
-                        {
-                            double speed = MathUtils.EuclideanDistance(
-                                NoteLaneExtension.GetSpawnerPosition(tapNote.Lane),
-                                NoteLaneExtension.GetSensorPosition(tapNote.Lane)) / TIME_NOTE_APPEARS;
-                            tapNote.Position = new Vector2(
-                                (float) (tapNote.Position.X + ((float)(speed * (-(float)Math.Cos((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) * (float)(Math.PI / 180)))) * Clock.ElapsedFrameTime)),
-                                (float) (tapNote.Position.Y + ((float)(speed * (-(float)Math.Sin((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) * (float)(Math.PI / 180)))) * Clock.ElapsedFrameTime))
-                                );
-                        }
-                        else
-                        {
-                            // Update the position
-                            note.Position += new Vector2(
-                                -(NOTE_SPEED * (float)Math.Cos((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) *
-                                                               (float)(Math.PI / 180))),
-                                -(NOTE_SPEED * (float)Math.Sin((NoteLaneExtension.GetAngle(tapNote.Lane) + 90f) *
-                                                               (float)(Math.PI / 180)))
-                            );
-                        }
-                    }
+                    updateTapNotePosition(tapNote);
                 }
             }
 
