b71ef73140c1268c432e410cb843b014b37ac417
pontakornth <pontakorn61@gmail.com>
2022-06-01 13:40:37 +0700

    Remove braces

--- maisim/maisim.Game/Scores/ScoreProcessor.cs
+++ maisim/maisim.Game/Scores/ScoreProcessor.cs
@@ -10,72 +10,45 @@ namespace maisim.Game.Scores
         public static ScoreRank CalculateRank(float accuracy)
         {
             if (accuracy >= 100.5f)
-            {
                 return ScoreRank.SSSPlus;
-            }
 
             if (accuracy >= 100)
-            {
                 return ScoreRank.SSS;
-            }
 
             if (accuracy >= 99.5f)
-            {
                 return ScoreRank.SSPlus;
-            }
 
             if (accuracy >= 99)
-            {
                 return ScoreRank.SS;
-            }
 
             if (accuracy >= 98)
-            {
                 return ScoreRank.SPlus;
-            }
 
             if (accuracy >= 97)
-            {
                 return ScoreRank.S;
-            }
 
             if (accuracy >= 94)
-            {
                 return ScoreRank.AAA;
-            }
 
             if (accuracy >= 90)
-            {
                 return ScoreRank.AA;
-            }
 
             if (accuracy >= 80)
-            {
                 return ScoreRank.A;
-            }
 
             if (accuracy >= 75)
-            {
                 return ScoreRank.BBB;
-            }
 
             if (accuracy >= 70)
-            {
                 return ScoreRank.BB;
-            }
 
             if (accuracy >= 60)
-            {
                 return ScoreRank.B;
-            }
 
             if (accuracy >= 50)
-            {
                 return ScoreRank.C;
-            }
 
             return ScoreRank.D;
-
         }
     }
 }
