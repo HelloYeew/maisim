4740cb5a03289b78142ec1bab60fe2a05fc10f50
Phawit Pornwattanakul <me@helloyeew.dev>
2022-06-17 01:41:41 +0700

    Add background to toolbar test scene

--- maisim/maisim.Game.Tests/Visual/Component/TestSceneToolbar.cs
+++ maisim/maisim.Game.Tests/Visual/Component/TestSceneToolbar.cs
@@ -1,13 +1,13 @@
 using maisim.Game.Graphics.UserInterface.Toolbar;
+using maisim.Game.Screen;
 
 namespace maisim.Game.Tests.Visual.Screen;
 
 public class TestSceneToolbar : maisimTestScene
 {
-    private osu.Framework.Screens.Screen toolbarScreen;
-
     public TestSceneToolbar()
     {
+        Add(new BackgroundScreenStack());
         Add(new Toolbar());
     }
 }
