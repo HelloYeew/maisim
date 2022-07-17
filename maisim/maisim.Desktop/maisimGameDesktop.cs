3b8a80d8c5644f5c06c91fb45a7818b017d6fede
Phawit Pornwattanakul <me@helloyeew.dev>
2022-07-18 00:44:02 +0700

    Change desktop window name

--- maisim/maisim.Desktop/maisimGameDesktop.cs
+++ maisim/maisim.Desktop/maisimGameDesktop.cs
@@ -5,6 +5,15 @@ namespace maisim.Desktop
 {
     public class maisimGameDesktop : maisimGame
     {
+        public override void SetHost(GameHost host)
+        {
+            base.SetHost(host);
+            var desktopWindow = (SDL2DesktopWindow)host.Window;
+
+            desktopWindow.Title = "maisim";
+        }
+
+
         protected override void LoadComplete()
         {
             base.LoadComplete();
